a096ee8bcd1e7f42fe7a7b8c1ed4d6ea50e3e270
Tymasc95 <tyler.mascitelli@gmail.com>
2025-04-20 23:40:27 -0400

    updated main.py to work correctly

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+
 def get_book_text(path):
     with open(path) as f:
         return f.read()   
-def main():
-    book_path = "workspace/github.com/bootdotdev/bookbot_project/bookbot/books/frankenstein.txtx"
-    text = get_book_text(book_path)
-    print(text)
-    
+
 main()
         
