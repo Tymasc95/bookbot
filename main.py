84ad75757b21bbb8b5f1669ab59d1c6293b42ea1
Tymasc95 <tyler.mascitelli@gmail.com>
2025-04-20 23:23:17 -0400

    updated main.py to read frankenstein into string

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()   
+def main():
+    book_path = "workspace/github.com/bootdotdev/bookbot_project/bookbot/books/frankenstein.txtx"
+    text = get_book_text(book_path)
+    print(text)
+    
+main()
+        
